5a214af28d4c924297c02fca670882ceb049a328
ouriel tapiero <oht862@gmail.com>
2023-08-09 11:20:16 +0300

    w

--- part_1.py
+++ part_1.py
@@ -9,171 +9,26 @@ from scipy.ndimage import maximum_filter
 from PIL import Image
 import matplotlib.pyplot as plt
 
-# if you wanna iterate over multiple files and json, the default source folder name is this.
-DEFAULT_BASE_DIR: str = 'INSERT_YOUR_DIR_WITH_PNG_AND_JSON_HERE'
 
-# The label we wanna look for in the polygons json file
-TFL_LABEL = ['traffic light']
+image = Image.open(r"C:\Users\ouriel\Downloads\kernel_7.png")
+image = image.convert('RGB')
 
-POLYGON_OBJECT = Dict[str, Union[str, List[int]]]
-RED_X_COORDINATES = List[int]
-RED_Y_COORDINATES = List[int]
-GREEN_X_COORDINATES = List[int]
-GREEN_Y_COORDINATES = List[int]
+image = np.array(image)
+print(image.shape)
+green_kernel = image[:, :, 1]
 
 
-def find_tfl_lights(c_image: np.ndarray,
-                    **kwargs) -> Tuple[RED_X_COORDINATES, RED_Y_COORDINATES, GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
-    """
-    Detect candidates for TFL lights. Use c_image, kwargs and you imagination to implement.
-
-    :param c_image: The image itself as np.uint8, shape of (H, W, 3).
-    :param kwargs: Whatever config you want to pass in here.
-    :return: 4-tuple of x_red, y_red, x_green, y_green.
-    """
-    ### WRITE YOUR CODE HERE ###
-    ### USE HELPER FUNCTIONS ###
-    return [500, 700, 900], [500, 550, 600], [600, 800], [400, 300]
-
-
-### GIVEN CODE TO TEST YOUR IMPLENTATION AND PLOT THE PICTURES
-def show_image_and_gt(c_image: np.ndarray, objects: Optional[List[POLYGON_OBJECT]], fig_num: int = None):
-    # ensure a fresh canvas for plotting the image and objects.
-    plt.figure(fig_num).clf()
-    # displays the input image.
-    plt.imshow(c_image)
-    labels = set()
-    if objects:
-        for image_object in objects:
-            # Extract the 'polygon' array from the image object
-            poly: np.array = np.array(image_object['polygon'])
-            # Use advanced indexing to create a closed polygon array
-            # The modulo operation ensures that the array is indexed circularly, closing the polygon
-            polygon_array = poly[np.arange(len(poly)) % len(poly)]
-            # gets the x coordinates (first column -> 0) anf y coordinates (second column -> 1)
-            x_coordinates, y_coordinates = polygon_array[:, 0], polygon_array[:, 1]
-            color = 'r'
-            plt.plot(x_coordinates, y_coordinates, color, label=image_object['label'])
-            labels.add(image_object['label'])
-        if 1 < len(labels):
-            # The legend provides a visual representation of the labels associated with the plotted objects.
-            # It helps in distinguishing different objects in the plot based on their labels.
-            plt.legend()
-
-
-def test_find_tfl_lights(image_path: str, image_json_path: Optional[str]=None, fig_num=None):
-    """
-    Run the attention code.
-    """
-    # using pillow to load the image
-    image: Image = Image.open(image_path)
-    # converting the image to a numpy ndarray array
-    c_image: np.ndarray = np.array(image)
-
-    objects = None
-    if image_json_path:
-        image_json = json.load(Path(image_json_path).open())
-        objects: List[POLYGON_OBJECT] = [image_object for image_object in image_json['objects']
-                                         if image_object['label'] in TFL_LABEL]
-
-    show_image_and_gt(c_image, objects, fig_num)
-
-    red_x, red_y, green_x, green_y = find_tfl_lights(c_image)
-    # 'ro': This specifies the format string. 'r' represents the color red, and 'o' represents circles as markers.
-    plt.plot(red_x, red_y, 'ro', markersize=4)
-    plt.plot(green_x, green_y, 'go', markersize=4)
-
-
-def main(argv=None):
-    """
-    It's nice to have a standalone tester for the algorithm.
-    Consider looping over some images from here, so you can manually examine the results.
-    Keep this functionality even after you have all system running, because you sometime want to debug/improve a module.
-
-    :param argv: In case you want to programmatically run this.
-    """
-
-    parser = argparse.ArgumentParser("Test TFL attention mechanism")
-    parser.add_argument('-i', '--image', type=str, help='Path to an image')
-    parser.add_argument("-j", "--json", type=str, help="Path to image json file -> GT for comparison")
-    parser.add_argument('-d', '--dir', type=str, help='Directory to scan images in')
-    args = parser.parse_args(argv)
-
-    # If you entered a custom dir to run from or the default dir exist in your project then:
-    directory_path: Path = Path(args.dir or DEFAULT_BASE_DIR)
-    if directory_path.exists():
-        # gets a list of all the files in the directory that ends with "_leftImg8bit.png".
-        file_list: List[Path] = list(directory_path.glob('*_leftImg8bit.png'))
-
-        for image in file_list:
-            # Convert the Path object to a string using as_posix() method
-            image_path: str = image.as_posix()
-            path: Optional[str] = image_path.replace('_leftImg8bit.png', '_gtFine_polygons.json')
-            image_json_path: Optional[str] = path if Path(path).exists() else None
-            test_find_tfl_lights(image_path, image_json_path)
-
-    if args.image and args.json:
-        test_find_tfl_lights(args.image, args.json)
-    elif args.image:
-        test_find_tfl_lights(args.image)
-    plt.show(block=True)
-
-
-def convolve_rgb_image(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
-    """
-    Convolves an RGB image with a given kernel.
-    :param image: The image to convolve.
-    :param kernel: The kernel to convolve the image with.
-    :return: The convolved image.
-    """
-    # extract the red, green and blue channels of the image
-    # and convolve each channel with the kernel separately.
-    # finally, stack the convolved channels to form the convolved image.
-    # mode='valid' means that there is no padding in the edges of the image.
-    # so the output image is smaller than the input.
-    # axis=2 means that the convolution is performed on the third axis of the image.
-    # (adding a third dimension to the image)
-    red = sg.correlate2d(image[:, :, 0], kernel, mode='valid')
-    green = sg.correlate2d(image[:, :, 1], kernel, mode='valid')
-    blue = sg.correlate2d(image[:, :, 2], kernel, mode='valid')
-    return np.stack([red, green, blue], axis=2)
-
-
-
-# if __name__ == '__main__':
-#     main()
-
-
-path = r"images_set/aachen_000016_000019_leftImg8bit.png"
-path2 =r"images_set/aachen_000011_000019_leftImg8bit.png"
-# open image
-image = Image.open(path2)
-# convert image to numpy array
-c_image = np.array(image)
-plt.imshow(c_image)
-plt.show()
-
-
-
-kernel = np.load(r"kernels/traffic_light_kernel.npy")
-green_kernel = np.array(kernel)
-green_kernel = green_kernel[:, :, 1]
-green_kernel = green_kernel.astype(np.float64)
-# convert the kernel to 0-1 values and normalize it to sum up to 0.
-green_kernel = green_kernel / np.sum(green_kernel)
-# make the kernel high-pass filter. (sum of the kernel values is 0)
 green_kernel = green_kernel - np.mean(green_kernel)
+green_kernel = green_kernel.astype(np.float32)
+green_kernel /= 255
+green_kernel -= np.mean(green_kernel)
+green_kernel /= np.max(green_kernel)
+print(np.sum(green_kernel))
+# save the kernel
+np.save(r"kernels\kernel_7.npy", green_kernel)
 
-green_channel = c_image[:, :, 0]
-green_channel[green_channel < 100] = 0
-green_channel = sg.convolve2d(green_channel, green_kernel, mode='same')
-green_channel[green_channel < 0] = 0
-fig=plt.figure()
-ax1 = fig.add_subplot(121)
-ax2 = fig.add_subplot(122)
-ax1.imshow(green_channel)
-ax2.imshow(c_image)
 
+
+plt.imshow(green_kernel)
 plt.show()
-
-
+print()
